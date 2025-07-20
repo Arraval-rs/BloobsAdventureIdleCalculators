09900da05eebbed03cf7212b8809445cc1300693
Arraval <cody.coombs@live.ca>
2025-07-19 23:03:58 -0230

    Interfaced UI with smithing skill

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,39 +1,68 @@
 <script setup lang="js">
-  const experienceSourceLabel = ref({
+  const experienceSource = ref({
     "label": "Select item to craft"
   })
 
-  const craftableItems = ref([
-    {
-      "label": "Bronze Bar",
-      "experience": 2
-    },
-    {
-      "label": "Bronze Axe",
-      "experience": 10
-    }
-  ])
+  const smithingSkill = ref(new smithing(0, 0, 0))
 
   const currentPrestige = ref(0)
   const currentLevel = ref(0)
   const currentExperience = ref(0)
 
-  const goalExperience = ref(0)
+  const goalLevel = ref(0)
 
   const calculatorOutput = ref([
     {
+      "experience source": "None",
       "experience required": 0,
       "experience per iteration" :0,
-      "required iterations": 0
+      "required iterations": 0,
+      "estimated time": "00:00:00",
+      "required materials": "N/A"
     }
   ])
 
   function calculateResults(event) {
-    const requiredExperience = goalExperience.value - currentExperience.value
-    const iterationExperience = experienceSourceLabel.value["experience"]
+    const goalExperience = smithingSkill.value.getExperience(goalLevel.value)
+    const requiredExperience = goalExperience - currentExperience.value
+    const iterationExperience = calculateIterationExperince(experienceSource.value["baseExperience"], currentPrestige.value)
+    const totalIterations = Math.ceil(requiredExperience/iterationExperience)
+    const materialString = ""
+
     calculatorOutput.value[0]["experience required"] = requiredExperience
     calculatorOutput.value[0]["experience per iteration"] = iterationExperience
-    calculatorOutput.value[0]["required iterations"] = requiredExperience/iterationExperience
+    calculatorOutput.value[0]["required iterations"] = totalIterations
+    calculatorOutput.value[0]["estimated time"] = calculateCraftingTime(totalIterations, experienceSource.value.baseCraftingTime)
+    calculatorOutput.value[0]["required materials"] = generateMaterialString(experienceSource.value.input, totalIterations)
+  }
+
+  function calculateCraftingTime(iterations, craftingTime) {
+    var duration = new Date(0)
+    duration.setSeconds(iterations * craftingTime)
+    try {
+      return duration.toISOString().substring(11, 19)
+    }
+    catch(err) {
+      console.error(err)
+      return "Error calulating time"
+    }
+    
+  }
+
+  function calculateIterationExperince(baseExperience, prestigeCount) {
+    var prestigeBonus = prestigeCount + 1
+    if (prestigeCount == 10) {
+      prestigeBonus = 15
+    }
+    return baseExperience * prestigeBonus
+  }
+
+  function generateMaterialString(inputMaterialsArray, iterations) {
+    var materialString = ""
+    for (const material of inputMaterialsArray) {
+      materialString += material.inputAmount * iterations + " " + material.name + "\n"
+    }
+    return materialString
   }
 
 </script>
@@ -49,10 +78,10 @@
     <UInputNumber v-model="currentExperience" placeholder="Enter current experience" />
   </UFormField>
   <UFormField label="Experience source">
-    <USelectMenu v-model="experienceSourceLabel" :items="craftableItems" class="w-48" />
+    <USelectMenu v-model="experienceSource" :items="smithingSkill.experienceSources" class="w-48" />
   </UFormField>
-  <UFormField label="Goal Experience">
-    <UInputNumber v-model="goalExperience" placeholder="Enter goal level" />
+  <UFormField label="Goal Level">
+    <UInputNumber v-model="goalLevel" placeholder="Enter goal level" />
   </UFormField>
   <UButton label="Submit" icon="i-lucide-calculator" @click="calculateResults"/>
   <UTable :data="calculatorOutput" class="flex-1" />
