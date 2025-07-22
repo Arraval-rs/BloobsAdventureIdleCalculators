0306d0a2a530a0050762c3e363186c30a6ca1755
Arraval <cody.coombs@live.ca>
2025-07-22 02:33:06 -0230

    Updatednuxt config for gh-pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/image', '@nuxt/ui'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  app: {
+    baseURL: '/BloobsAdventureIdleCalculators/',
+    buildAssetsDir: 'assets'
+  }
 })
